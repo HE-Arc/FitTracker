539c0db859fb7ca1ab4b30709ebb2d84a7448cb6
Gabriel Fasano <gabriel.fasano@he-arc.ch>
2022-02-27 12:19:37 +0100

    #4 Authentication system done, create template login and register, create base_layout to extend template

--- fittrackerproject/fittrackerapp/urls.py
+++ fittrackerproject/fittrackerapp/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import include, path
+from . import views
+from django.contrib.auth.forms import UserCreationForm
 
+from .models import *
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name = "home"),
+    path('index', views.index, name = "index"),
+    path('register', views.register_view, name = "register"),
+    path('login', views.login_view, name = "login"),
+    path('logout', views.logout_view, name='logout'),
+    path('generator', views.generator_view, name = "generator"),
 ]
--- fittrackerproject/fittrackerapp/views.py
+++ fittrackerproject/fittrackerapp/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're logged  in.")
+
+def home(request):
+    return render(request,"home.html")
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST) # Pass information from form with request.POST
+        if form.is_valid(): # Check if form is valid or not (User exist ? Password ok ? etc.)
+            user = form.save() # Log the user in
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm() # Create a new instance of this form
+    return render(request,"register.html", {'form':form}) # Send the UserCreationForm to render
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST) # Pass information from form with request.POST
+        if form.is_valid(): # Check if form is valid or not (User exist ? Password ok ? etc.)
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+        else:
+            return redirect('home')
+    else:
+        form = AuthenticationForm() # Create a new instance of this form
+    return render(request,"login.html", {'form':form}) # Send the UserCreationForm to render
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('index')
+
+@login_required(login_url="login")
+def generator_view(request):
+    return HttpResponse("Generator page")
