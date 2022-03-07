2d30c3f73da253a311bbe42e26db43a44c8e87ec
Gabriel Fasano <gabriel.fasano@he-arc.ch>
2022-03-07 22:29:20 +0100

    #14 WIP training form, refactor model (/!\)

--- fittrackerproject/fittrackerapp/models.py
+++ fittrackerproject/fittrackerapp/models.py
@@ -2,39 +2,47 @@ from django.conf import settings
 from django.db import models
 
 
-class Program(models.Model):
-    name = models.CharField(max_length=50)
-    discipline = models.ForeignKey('Discipline', on_delete=models.CASCADE)
-
-class User_Program(models.Model):
-    user = models.ManyToManyField(settings.AUTH_USER_MODEL)
-    program = models.ManyToManyField(Program)
-
 class Discipline(models.Model):
     name = models.CharField(max_length=50)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Program(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    discipline = models.ForeignKey('Discipline', on_delete=models.CASCADE)
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=50)
+    rank_in_program = models.IntegerField()
+    number_of_set = models.IntegerField()
+    label_data = models.CharField(max_length=50)
+    indication = models.CharField(max_length=50)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    program = models.ManyToManyField(Program, through='Exercise_Program')
+
+
+class Exercise_Program(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+
 
 class Training(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     program = models.ForeignKey('Program', on_delete=models.CASCADE)
 
-class Exercise(models.Model):
-    name = models.CharField(max_length=50)
-    program = models.ForeignKey('Program', on_delete=models.CASCADE)
-    rank_in_program = models.IntegerField
-    number_of_set = models.IntegerField
-    label_data = models.CharField(max_length=50)
-    indication = models.CharField(max_length=50)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-
-class Exercise_Program(models.Model):
-    exercise = models.ManyToManyField('Exercise')
-    program = models.ManyToManyField('Program')
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
 
 class Data(models.Model):
     training = models.ForeignKey('Training', on_delete=models.CASCADE)
     exercise = models.ForeignKey('Exercise', on_delete=models.CASCADE)
-    rank_in_set = models.IntegerField
-    value = models.IntegerField
+    rank_in_set = models.IntegerField()
+    value = models.IntegerField()
+
+
+# class User_Program(models.Model):
+#     user = models.ManyToManyField(settings.AUTH_USER_MODEL)
+#     program = models.ManyToManyField(Program)
