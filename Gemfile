322119bbc352ac9cd5d0ed73c1d42dd8e7b0c212
Badel Kevin <kevin.badel@he-arc.ch>
2022-03-13 09:33:56 +0100

    #5 add label traduction

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'capistrano'
-
-gem 'ed25519'
-gem 'bcrypt_pbkdf'
--- fittrackerproject/fittrackerapp/forms.py
+++ fittrackerproject/fittrackerapp/forms.py
@@ -1,4 +1,5 @@
 from dataclasses import field
+from pyexpat import model
 from django.forms import ModelForm
 from .models import Exercise,Program
 
@@ -6,9 +7,23 @@ class ExerciceForm(ModelForm):
     class Meta:
         model=Exercise
         fields=['name','category','number_of_set','rank_in_program','label_data','indication','program',]
+        labels = {
+        'name': 'Nom',
+        'category': 'Catégorie',
+        'number_of_set': 'Nombre de série',
+        'rank_in_program': 'Nombre de répétitions',
+        'label_data': 'Label_data',
+        'indication': 'Indication',
+        'program': 'Programme',
+        }
+        
         
 class ProgramForm(ModelForm):
     class Meta:
         model=Program
         fields=['name','discipline']
+        labels = {
+        'name': 'Nom',
+        'discipline': 'Discipline',
+        }
         
