0a8300cac89b7cea0c23cd14408f24e7f1fd1f0f
Gabriel Fasano <gabriel.fasano@he-arc.ch>
2022-03-14 21:49:16 +0100

    #14 WIP add form for exercise, fix models, improved design

--- fittrackerproject/fittrackerapp/forms.py
+++ fittrackerproject/fittrackerapp/forms.py
@@ -1,5 +1,23 @@
 from django import forms
-
+from fittrackerapp.models import *
 
 class ExerciseForm(forms.Form):
-    template_exercise = 'form_snipper.html'
+    def __init__(self,*args,**kwargs):
+        label = kwargs.pop('label')
+        number_of_set = kwargs.pop('number_of_set')
+
+        super(ExerciseForm, self).__init__(*args,**kwargs)
+
+        for i in range(number_of_set):
+            self.fields['input'+ str(i)] = forms.IntegerField(required=True, label=label)
+            self.fields['input'+ str(i)].widget.attrs.update({'class': 'form-control form'})
+
+    def save(self, exercise_id, current_training):
+        data = self.cleaned_data
+
+        for index, value in enumerate(data):
+            saving = Data(rank_in_set=data['input'+str(index)], value=value, exercise_id=exercise_id, training_id=current_training)
+            saving.save()
+
+
+
