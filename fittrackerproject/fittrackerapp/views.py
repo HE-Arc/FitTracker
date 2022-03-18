ca2ac8779242def66b000503ed4b3d3affe7b981
Gabriel Fasano <gabriel.fasano@he-arc.ch>
2022-03-18 23:14:17 +0100

    #14 User can fill his training

--- fittrackerproject/fittrackerapp/views.py
+++ fittrackerproject/fittrackerapp/views.py
@@ -1,30 +1,37 @@
+import re
 from django.shortcuts import render, redirect
 
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from fittrackerapp.models import *
 from fittrackerapp.forms import *
 
+
 def home(request):
-    return render(request,"home.html")
+    return render(request, "home.html")
+
 
 def register_view(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST) # Pass information from form with request.POST
-        if form.is_valid(): # Check if form is valid or not (User exist ? Password ok ? etc.)
-            user = form.save() # Log the user in
+        # Pass information from form with request.POST
+        form = UserCreationForm(request.POST)
+        if form.is_valid():  # Check if form is valid or not (User exist ? Password ok ? etc.)
+            user = form.save()  # Log the user in
             login(request, user)
             return redirect('dashboard')
     else:
-        form = UserCreationForm() # Create a new instance of this form
-    return render(request,"register.html", {'form':form}) # Send the UserCreationForm to render
+        form = UserCreationForm()  # Create a new instance of this form
+    # Send the UserCreationForm to render
+    return render(request, "register.html", {'form': form})
+
 
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST) # Pass information from form with request.POST
-        if form.is_valid(): # Check if form is valid or not (User exist ? Password ok ? etc.)
+        # Pass information from form with request.POST
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():  # Check if form is valid or not (User exist ? Password ok ? etc.)
             user = form.get_user()
             login(request, user)
             if 'next' in request.POST:
@@ -32,8 +39,9 @@ def login_view(request):
         else:
             return redirect('dashboard')
     else:
-        form = AuthenticationForm() # Create a new instance of this form
-    return render(request,"login.html", {'form':form}) # Send the UserCreationForm to render
+        form = AuthenticationForm()  # Create a new instance of this form
+    # Send the UserCreationForm to render
+    return render(request, "login.html", {'form': form})
 
 
 def logout_view(request):
@@ -43,35 +51,39 @@ def logout_view(request):
 
 
 @login_required(login_url="login")
-def generator_view(request):
-    return HttpResponse("Generator page")
-
-@login_required(login_url="login")
 def dashboard_view(request):
     program_list = Program.objects.filter(owner=request.user.id)
-    training_list = Training.objects.all()
 
-    return render(request,"dashboard.html", {'program_list':program_list, 'training_list':training_list})
+    return render(request, "dashboard.html", {'program_list': program_list})
+
 
 @login_required(login_url="login")
-def training_index_view(request, id):
+def training_view(request, id):
     exercises_list = Exercise.objects.filter(exercise_program__program_id=id)
-    training = Training(program_id=1)
-    training.save()
-    id = training.id
-    request.session['current_training'] = id
-    return render(request,"training_index.html", {'exercises_list': exercises_list})
+    request.session['program_id'] = id
+    if 'dashboard' in request.META['HTTP_REFERER']:
+        request.session['first'] = 1
+    return render(request, "training_index.html", {'exercises_list': exercises_list})
+
 
 @login_required(login_url="login")
 def exercise_view(request, id):
     if request.method == "POST":
         exercise = Exercise.objects.get(exercise_program__exercise_id=id)
-        form = ExerciseForm(request.POST, label="Poids", number_of_set=exercise.number_of_set) # Pass information from form with request.POST
+        # Pass information from form with request.POST
+        form = ExerciseForm(request.POST, label="Poids",
+                            number_of_set=exercise.number_of_set)
         if form.is_valid():
-            exercise = form.save(exercise.id, request.session['current_training'])
-        return redirect('dashboard')
+            if request.session['first'] == 1:
+                training = Training(program_id=request.session['program_id'])
+                training.save()
+                request.session['first'] = 0
+                request.session['training_id'] = training.id
+
+            exercise = form.save(exercise.id, request.session['training_id'])
+        return redirect('/training/' + str(request.session['program_id']))
     else:
         exercise = Exercise.objects.get(exercise_program__exercise_id=id)
-        form = ExerciseForm(label=exercise.label_data, number_of_set=exercise.number_of_set)
-        return render(request,"exercise.html", {'exercise': exercise, 'form': form})
-
+        form = ExerciseForm(label=exercise.label_data,
+                            number_of_set=exercise.number_of_set)
+        return render(request, "exercise.html", {'exercise': exercise, 'form': form})
