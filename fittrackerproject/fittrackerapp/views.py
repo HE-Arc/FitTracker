0380ec48cd09f8232f99b39bad6f417564151338
Gabriel Fasano <gabriel.fasano@he-arc.ch>
2022-03-23 09:47:08 +0100

    #9 We can not access an exercise if it's already done + protection back-end

--- fittrackerproject/fittrackerapp/views.py
+++ fittrackerproject/fittrackerapp/views.py
@@ -1,7 +1,7 @@
 import re
 from django.shortcuts import render, redirect
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
@@ -62,25 +62,23 @@ def program_view(request, id):
         Training.objects.filter(
             id=request.session['training_id']).update(validated=True)
         del request.session['training_id']
-        redirect('/dashboard')
+        return redirect('dashboard')
     else:
-        exercises_list = Exercise.objects.filter(
-            exercise_program__program_id=id)
+        exercises_list = Exercise.objects.filter(exercise_program__program_id=id)
         request.session['program_id'] = id
 
         if 'training_id' not in request.session:
             is_done = []
         else:
-            # TODO si l'exercice n'est pas présent il renvoie None ! Faire en sorte qu'il ne soit pas enregistré !
-            is_done = [Data.objects.filter(training_id=request.session['training_id'],
-                                           exercise_id=exercise.id).first() for exercise in exercises_list]
+            is_done = [Data.objects.filter(training_id=request.session['training_id'], exercise_id=exercise.id).first()
+                       for exercise in exercises_list]
             if len(is_done) != 0:
-                is_done = [i.exercise_id for i in is_done]
+                is_done = [i.exercise_id for i in is_done if i is not None]
 
-    if 'dashboard' in request.META['HTTP_REFERER']:
-        request.session['first'] = 1
+        if 'dashboard' in request.META['HTTP_REFERER']:
+            request.session['first'] = 1
 
-    return render(request, "training_index.html", {'exercises_list': exercises_list, 'is_done': is_done})
+        return render(request, "training_index.html", {'exercises_list': exercises_list, 'is_done': is_done})
 
 
 @login_required(login_url="login")
@@ -100,6 +98,10 @@ def exercise_view(request, id):
             exercise = form.save(exercise.id, request.session['training_id'])
         return redirect('/program/' + str(request.session['program_id']))
     else:
+        if 'training_id' in request.session:
+            already_done = Data.objects.filter(training_id=request.session['training_id'], exercise_id=id).first()
+            if already_done != None:
+                return HttpResponseForbidden()
         exercise = Exercise.objects.get(exercise_program__exercise_id=id)
         form = ExerciseForm(label=exercise.label_data,
                             number_of_set=exercise.number_of_set)
