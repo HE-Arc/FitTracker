53a5faf024925eb9b2096d255c4dbacd05937461
Kevin Badel <kevin.badel@he-arc.ch>
2022-04-05 23:05:03 +0200

    fix probleme with last merge

---
+++ fittrackerproject/fittrackerapp/migrations/0002_alter_exercise_rank_in_program.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fittrackerapp', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='exercise',
+            name='rank_in_program',
+            field=models.IntegerField(default=0),
+        ),
+    ]
